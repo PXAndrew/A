1ad91443fa391e692290026100d4ca60033741de
PXAndrew <82198066@qq.com>
2017-03-23 11:45:28 +0800

    add demo and pod

--- A/ViewController.m
+++ A/ViewController.m
@@ -7,22 +7,51 @@
 //
 
 #import "ViewController.h"
+#import <HandyFrame/UIView+LayoutMethods.h>
 
 @interface ViewController ()
+
+@property (strong, nonatomic) UIButton *button;
 
 @end
 
 @implementation ViewController
 
+#pragma mark - life cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    [self.view addSubview:self.button];
+}
+
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    
+    
+    self.button.size = CGSizeMake(100, 100);
+    [self.button centerEqualToView:self.view];
+    
+}
+
+#pragma mark - event reponds
+- (void)didTappedButton:(UIButton *)button {
+    
+    
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - getters and setters
+- (UIButton *)button {
+    if (!_button) {
+        _button = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_button setTitle:@"show A" forState:UIControlStateNormal];
+        [_button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [_button addTarget:self action:@selector(didTappedButton:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _button;
 }
 
 
